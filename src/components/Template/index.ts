561467c8fa70535d67f98030facc96411274643e
yuanlee <yuanlee@163.com>
2023-03-01 19:12:39 +0800

    add: 组件模板

---
+++ src/components/Template/index.ts
@@ -0,0 +1,3 @@
+import Temp from "./src/index.vue";
+
+export default Temp;
---
+++ src/components/Template/src/typing.ts
@@ -0,0 +1,4 @@
+import type { ExtractPropTypes } from "vue";
+
+export const props = {};
+export type Props = ExtractPropTypes<typeof props>;
