79491b87eec3ae564cac95fa47de781a38200664
yuanlee <yuanlee@163.com>
2023-03-02 09:36:20 +0800

    test: fit

--- src/views/avatar.vue
+++ src/views/avatar.vue
@@ -1,13 +1,51 @@
 <template>
-  <LeAvatar>User</LeAvatar>
-  <LeAvatar src="/logo2.svg">User</LeAvatar>
-  <LeAvatar src="/logo3.svg" @error="() => {}">User</LeAvatar>
-  <LeAvatar src="/logo4.svg" @error="() => {}">
-    <img src="/logo.svg">
-  </LeAvatar>
+  <dl class="demo">
+    <dt>Basic</dt>
+    <dd>
+      <LeAvatar>User</LeAvatar>
+      <LeAvatar src="/vue.png">Vue</LeAvatar>
+      <LeAvatar src="/react.png">React</LeAvatar>
+    </dd>
+  </dl>
+
+  <dl class="demo">
+    <dt>Error</dt>
+    <dd>
+      <LeAvatar src="/logo.svg" @error="() => {}">User</LeAvatar>
+      <LeAvatar src="/logo.svg">User2</LeAvatar>
+      <LeAvatar src="/logo.svg" @error="() => {}">
+        <img src="/fallback.png">
+      </LeAvatar>
+    </dd>
+  </dl>
+
+  <dl class="demo">
+    <dt>Fit</dt>
+    <dd>
+      <LeAvatar src="/vue.png" fit="scale-down">Vue</LeAvatar>
+      <LeAvatar src="/react.png" fit="fill">React</LeAvatar>
+    </dd>
+  </dl>
+
+  <dl class="demo">
+    <dt>Square</dt>
+    <dd>
+      <LeAvatar src="/vue.png" shape="square">Vue</LeAvatar>
+      <LeAvatar shape="square">User</LeAvatar>
+    </dd>
+  </dl>
+
+  <dl class="demo">
+    <dt>Size</dt>
+    <dd>
+      <LeAvatar src="/vue.png" size="large">Vue</LeAvatar>
+      <LeAvatar size="80">User</LeAvatar>
+    </dd>
+  </dl>
+
 </template>
 
 <script lang="ts" setup>
-import { reactive, ref } from "vue";
+import {reactive, ref} from "vue";
 import LeAvatar from "@/components/LeAvatar";
 </script>
