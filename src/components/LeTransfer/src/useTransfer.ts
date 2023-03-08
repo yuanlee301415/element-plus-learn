d4e70e78505b270a32db1c05f3dbe002c8da04ad
yuanlee <yuanlee@163.com>
2023-03-08 16:47:49 +0800

    stash: 显示左侧列表

--- src/components/LeTransfer/src/useTransfer.ts
+++ src/components/LeTransfer/src/useTransfer.ts
@@ -1,12 +1,19 @@
 import type { SetupContext} from "vue";
 import type { TransferKey, TransferProps, TransferEmit, Option } from "@/components/LeTransfer/src/typing";
 
-import {ref} from "vue";
+import {computed, ref} from "vue";
 import {UPDATE_MODEL_EVENT} from "@/constants/event";
 
 export const useTransfer = (props: TransferProps, emit: SetupContext<TransferEmit>['emit']) => {
-    const leftItems = ref<Option[]>([]) // 数组中的各项 key 不包含在父组件的 modelValue 中
-    const rightItems = ref<Option[]>([]) // 数组中的各项 key 包含在父组件的 modelValue 中
+    // const leftItems = ref<Option[]>([]) // 数组中的各项 key 不包含在父组件的 modelValue 中
+    const leftItems = computed(() => {
+        return props.data?.filter(item => !props.modelValue?.includes(item.key))
+    })
+
+    // const rightItems = ref<Option[]>([]) // 数组中的各项 key 包含在父组件的 modelValue 中
+    const rightItems = computed(() => {
+        return props.data?.filter(item => props.modelValue?.includes(item.key))
+    })
 
     const leftSelectedKeys = ref<TransferKey[]>([]) // 左侧面板选中项的 key
     const rightSelectedKeys = ref<TransferKey[]>([]) // 右侧面板选中项的 key
