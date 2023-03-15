78dbe2f9e010bb215786f88b4ebb3e64005b7adf
yuanlee <yuanlee@163.com>
2023-03-15 19:56:42 +0800

    fix: formatDate

--- src/utils/date.ts
+++ src/utils/date.ts
@@ -1,44 +1,45 @@
-const ONLY_TIME = 0;
-const ONLY_DATE = 1;
-const DATE_TIME = 2;
-
-export type FORMAT_DATE_TYPE =
-  | typeof ONLY_TIME
-  | typeof ONLY_DATE
-  | typeof DATE_TIME;
+const enum FORMAT_DATE_TYPE {
+  ONLY_TIME,
+  ONLY_DATE,
+  DATE_TIME,
+}
 
 export function formatDate(
   value: Date | number | string,
-  type: FORMAT_DATE_TYPE = ONLY_DATE,
-  separator = "/"
+  type: FORMAT_DATE_TYPE = FORMAT_DATE_TYPE.ONLY_DATE,
+  dateSeparator: string = "/",
+  timeSeparator: string = ":",
+  stringSeparator: string = " ",
+  pad: string = "0",
+  nil: string = ""
 ): string {
   value = new Date(value);
-  if (!value.getTime?.()) return "";
+  if (!value.getTime?.()) return nil;
 
-  const [year, month, date, hour, min, sec] = [
-    value.getFullYear(),
+  const year = value.getFullYear();
+  const [month, date, hour, min, sec] = [
     value.getMonth() + 1,
     value.getDate(),
     value.getHours(),
     value.getMinutes(),
     value.getSeconds(),
-  ].map((_) => ("0" + _).slice(0, 2));
+  ].map((_) => (pad + _).slice(-2));
 
   switch (type) {
-    case 0:
-      return [hour, min, sec].join(":");
-    case 1:
-      return [year, month, date].join(separator);
+    case FORMAT_DATE_TYPE.ONLY_TIME:
+      return [hour, min, sec].join(timeSeparator);
+    case FORMAT_DATE_TYPE.ONLY_DATE:
+      return [year, month, date].join(dateSeparator);
     default:
       return [
-        [year, month, date].join(separator),
-        [hour, min, sec].join(":"),
-      ].join(" ");
+        [year, month, date].join(dateSeparator),
+        [hour, min, sec].join(timeSeparator),
+      ].join(stringSeparator);
   }
 }
-formatDate.FORMAT_DATE_TYPE__ONLY_TIME = ONLY_TIME as FORMAT_DATE_TYPE;
-formatDate.FORMAT_DATE_TYPE__ONLY_DATE = ONLY_DATE as FORMAT_DATE_TYPE;
-formatDate.FORMAT_DATE_TYPE__DATE_TIME = DATE_TIME as FORMAT_DATE_TYPE;
+formatDate.FORMAT_DATE_TYPE__ONLY_TIME = FORMAT_DATE_TYPE.ONLY_TIME;
+formatDate.FORMAT_DATE_TYPE__ONLY_DATE = FORMAT_DATE_TYPE.ONLY_DATE;
+formatDate.FORMAT_DATE_TYPE__DATE_TIME = FORMAT_DATE_TYPE.DATE_TIME;
 
 export function countdown(msecs: number): string {
   const SEC = 1000;
