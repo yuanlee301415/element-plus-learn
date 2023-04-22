10809ae47bb9ed5396d1a01638137aeabecc6b77
yuanlee <yuanlee@163.com>
2023-04-22 18:23:14 +0800

    chore: 升级 Vue3 初始化依赖到最新版本

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,18 +1,22 @@
 /* eslint-env node */
-require("@rushstack/eslint-patch/modern-module-resolution");
+require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  extends: [
-    "plugin:vue/vue3-essential",
-    "eslint:recommended",
-    "@vue/eslint-config-typescript",
-    "@vue/eslint-config-prettier",
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
   ],
   parserOptions: {
-    ecmaVersion: "latest",
+    ecmaVersion: 'latest'
   },
   rules: {
-    "vue/multi-word-component-names": 0,
+    "vue/multi-word-component-names": 0
   },
-};
+  "globals": {
+    "__APP_VERSION__": "readonly",
+    "__APP_BUILD_TIME__": "readonly"
+  }
+}
