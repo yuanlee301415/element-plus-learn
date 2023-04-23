14626719d5a72d404c5da0ad9d80730fd1897e1c
yuanlee <yuanlee@163.com>
2023-04-23 21:42:27 +0800

    clear

--- src/components/LeInput/src/index.vue
+++ src/components/LeInput/src/index.vue
@@ -1,26 +1,29 @@
 <template>
-  <div :class="inputCls" class="input">
+  <div :class="inputCls" class="input" @mouseenter="handleEnter" @mouseleave="handleLeave">
     <div :class="wrapperCls" class="input__wrapper">
 
       <span v-if="props.prefixIcon" class="input__prefix">
         <span class="input__prefix-inner">
           <slot name="prefix"/>
-          <icon :name="props.prefixIcon" class="input__icon"/>
+          <Icon :name="props.prefixIcon" class="input__icon"/>
         </span>
       </span>
 
       <input
-          :disabled="props.disabled"
+          ref="inputRef"
           type="text"
           class="input__inner"
+          :disabled="props.disabled"
           @input="handleInput"
+          @change="handleChange"
           @focus="handleFocus"
           @blur="handleBlur"
       >
-      <span v-if="props.suffixIcon" class="input__suffix">
+      <span v-if="props.suffixIcon || props.clearable" class="input__suffix">
         <span class="input__suffix-inner">
           <slot name="suffix"/>
-          <icon :name="props.suffixIcon" class="input__icon"/>
+          <Icon v-if="props.suffixIcon" :name="props.suffixIcon" class="input__icon"/>
+          <Icon v-if="props.clearable" v-show="clearIconVisible" name="circle-close" class="input__icon input__clear" @click="handleClear" @mousedown.prevent/>
         </span>
       </span>
     </div>
@@ -28,29 +31,60 @@
 </template>
 
 <script setup lang="ts">
-import { inputProps, inputEmit } from "./input";
-import {useInput} from "@/components/LeInput/src/useInput";
-import {computed, reactive, ref} from "vue";
+import {inputEmit, inputProps} from "./input";
+import {computed, nextTick, reactive, ref, watch} from "vue";
+import {CHANGE_EVENT, INPUT_EVENT, UPDATE_MODEL_EVENT} from "@/constants/event";
 
 const props = defineProps(inputProps)
 const emit = defineEmits(inputEmit)
 
-const { handleInput } = useInput(props, emit)
+const inputRef = ref<HTMLInputElement>()
 const inputCls = computed(() => ({
   'is-disabled': props.disabled
 }))
-
 const wrapperCls = reactive({
   'is-focus': false
 })
+const clearIconVisible = ref(false)
+const nativeInputValue = computed(() => props.modelValue ? String(props.modelValue) : '')
 
+async function handleInput (event: Event){
+  const { value }= event.target as HTMLInputElement
+  clearIconVisible.value = true
+  emit(UPDATE_MODEL_EVENT, value)
+  emit(INPUT_EVENT, value)
+  await nextTick()
+  setNativeInputValue()
+}
+function handleChange(event: Event) {
+  const { value } = event.target as HTMLInputElement
+  emit(CHANGE_EVENT, value)
+}
+function setNativeInputValue() {
+  inputRef.value!.value = nativeInputValue.value
+}
 function handleFocus() {
   wrapperCls["is-focus"] = true
 }
 function handleBlur() {
+  clearIconVisible.value = false
   wrapperCls["is-focus"] = false
 }
+function handleEnter() {
+  if (!props.modelValue) return
+  clearIconVisible.value = true
+}
+function handleLeave() {
+  clearIconVisible.value = false
+}
+function handleClear() {
+  clearIconVisible.value = false
+  emit(UPDATE_MODEL_EVENT, '')
+  emit(INPUT_EVENT, '')
+  emit(CHANGE_EVENT, '')
+}
 
+watch(nativeInputValue, () => setNativeInputValue())
 </script>
 
 <style>
--- src/components/LeInput/src/input.ts
+++ src/components/LeInput/src/input.ts
@@ -1,6 +1,6 @@
 import type { ExtractPropTypes } from 'vue'
 
-import { UPDATE_MODEL_EVENT } from '@/constants/event'
+import { UPDATE_MODEL_EVENT, INPUT_EVENT, CHANGE_EVENT } from '@/constants/event'
 
 export const inputProps = {
   modelValue: String,
@@ -12,7 +12,9 @@ export const inputProps = {
 export type InputProps = ExtractPropTypes<typeof inputProps>
 
 export const inputEmit = {
-  [UPDATE_MODEL_EVENT]: (val: string) => typeof val === 'string'
+  [UPDATE_MODEL_EVENT]: (val: string) => typeof val === 'string',
+  [INPUT_EVENT]: (val: string) => typeof val === 'string',
+  [CHANGE_EVENT]: (val: string) => typeof val === 'string',
 }
 
 export type InputEmit = typeof inputEmit
